f43391c10a4a6b897dde6b57ad897cd0afd98c0c
Mirko Thalmann <mirkothalmann@hotmail.com>
2025-01-17 18:06:01 +0100

    initialize method jointly fitting average and individual differences

--- models/model.py
+++ models/model.py
@@ -2,23 +2,24 @@
 # -*- coding: utf-8 -*-
 
 __all__ = [
-            'SPoSE',
-            'l1_regularization',
-            ]
+    'SPoSE',
+    'l1_regularization',
+]
 
 import re
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SPoSE(nn.Module):
 
     def __init__(
-                self,
-                in_size:int,
-                out_size:int,
-                init_weights:bool=True,
-                ):
+        self,
+        in_size: int,
+        out_size: int,
+        init_weights: bool = True,
+    ):
         super(SPoSE, self).__init__()
         self.in_size = in_size
         self.out_size = out_size
@@ -27,7 +28,7 @@ class SPoSE(nn.Module):
         if init_weights:
             self._initialize_weights()
 
-    def forward(self, x:torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.fc(x)
 
     def _initialize_weights(self) -> None:
@@ -36,12 +37,14 @@ class SPoSE(nn.Module):
             if isinstance(m, nn.Linear):
                 m.weight.data.normal_(mean, std)
 
+
 def l1_regularization(model) -> torch.Tensor:
     l1_reg = torch.tensor(0., requires_grad=True)
     for n, p in model.named_parameters():
         if re.search(r'weight', n):
             l1_reg = l1_reg + torch.norm(p, 1)
     return l1_reg
+
 
 class IndependentLinearLayerWithoutIntercept(nn.Module):
     # can be extended with intercept if required, see commented part below
@@ -50,11 +53,12 @@ class IndependentLinearLayerWithoutIntercept(nn.Module):
         # Define a parameter for each input dimension for weights
         self.weights = nn.Parameter(torch.ones(input_dim))
         # Define a parameter for each input dimension for intercepts
-        #self.intercepts = nn.Parameter(torch.zeros(input_dim))
+        # self.intercepts = nn.Parameter(torch.zeros(input_dim))
 
     def forward(self, x):
         # Scale each input dimension by its corresponding weight and add the intercept
-        return x * self.weights# + self.intercepts
+        return x * self.weights  # + self.intercepts
+
 
 class IndependentLinearLayerWithIntercept(nn.Module):
     # can be extended with intercept if required, see commented part below
@@ -68,3 +72,31 @@ class IndependentLinearLayerWithIntercept(nn.Module):
     def forward(self, x):
         # Scale each input dimension by its corresponding weight and add the intercept
         return x * self.weights + self.intercepts
+
+
+class SPoSE_ID(nn.Module):
+    def __init__(
+        self,
+        in_size: int,
+        out_size: int,
+        num_participants: int,
+        init_weights: bool = True,
+    ):
+
+        super(SPoSE, self).__init__()
+        self.in_size = in_size
+        self.out_size = out_size
+        self.fc = nn.Linear(self.in_size, self.out_size, bias=False)
+        self.individual_weights = nn.Embedding(num_participants, self.out_size)
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x: torch.Tensor, id: torch.Tensor) -> torch.Tensor:
+        w_i = self.individual_weights(id)
+        return w_i * self.fc(x)
+
+    def _initialize_weights(self) -> None:
+        mean, std = .1, .01
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.normal_(mean, std)
