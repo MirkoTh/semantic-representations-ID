b19f9835dad1fb0fdce5667c4908ea3bbdac10b7
Mirko Thalmann <mirko.thalmann@helmholtz-munich.de>
2025-07-07 18:09:59 +0200

    select top disagreed triplets for study

---
+++ experiments/expt-utils/properImageList.js
@@ -0,0 +1,37 @@
+const properImageList = [
+  {
+    images_proper: [
+      "../data/images/belt_buckle/belt_buckle_01b.jpg",
+      "../data/images/juicer2/juicer2_01b.jpg",
+      "../data/images/tomato_sauce/tomato_sauce_01b.jpg",
+    ],
+  },
+  {
+    images_proper: [
+      "../data/images/coconut/coconut_01b.jpg",
+      "../data/images/curry/curry_01b.jpg",
+      "../data/images/stove1/stove1_01b.jpg",
+    ],
+  },
+  {
+    images_proper: [
+      "../data/images/cereal/cereal_01b.jpg",
+      "../data/images/vending_machine/vending_machine_01b.jpg",
+      "../data/images/wallpaper/wallpaper_01b.jpg",
+    ],
+  },
+  {
+    images_proper: [
+      "../data/images/raisin/raisin_01b.jpg",
+      "../data/images/shaving_cream/shaving_cream_01b.jpg",
+      "../data/images/vending_machine/vending_machine_01b.jpg",
+    ],
+  },
+  {
+    images_proper: [
+      "../data/images/ice-cream_cone/ice-cream_cone_01b.jpg",
+      "../data/images/life_jacket/life_jacket_01b.jpg",
+      "../data/images/spinach/spinach_01b.jpg",
+    ],
+  },
+];
