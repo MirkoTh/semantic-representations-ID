e1c83e8410239b957e9114166fef0205f697db5c
MirkoTh <mirkothalmann@hotmail.com>
2025-08-06 16:00:19 +0200

    save ooo ID results in modeling format

--- load-study-results.R
+++ load-study-results.R
@@ -14,6 +14,38 @@ tbl_comprehension <- jsonlite::fromJSON("data/study1-2025-08/comprehension-check
 tbl_ooo <- jsonlite::fromJSON("data/study1-2025-08/odd-one-out.json") %>%
   as_tibble()
 
+# format for modeling: anchor pos neg ID
+# saved as .txt
+tbl_ooo_ids <- tbl_ooo %>% unnest(stimulus_ids) %>%
+  mutate(stimulus_loc = rep(c("ID1", "ID2", "ID3"), nrow(.)/3)) %>%
+  pivot_wider(names_from = stimulus_loc, values_from = stimulus_ids)
+tbl_ooo_ids <- tbl_ooo_ids %>% rowwise() %>% mutate(
+  idx_odd = which(1:3 == response + 1),
+  which_not_odd = list(c(1, 2, 3)[-idx_odd])
+) %>% unnest(which_not_odd) %>%
+  mutate(idx_not_odd = rep(c("idx_positive", "idx_negative"), nrow(.)/2)) %>%
+  pivot_wider(names_from = idx_not_odd, values_from = which_not_odd) %>%
+  relocate(idx_positive, .before = idx_odd) %>%
+  relocate(idx_negative, .before = idx_odd)
+
+tbl_ooo_ID_save <- tbl_ooo_ids %>% 
+  rowwise() %>%
+  mutate(
+    positive = c(ID1, ID2, ID3)[idx_positive],
+    negative = c(ID1, ID2, ID3)[idx_negative],
+    odd = c(ID1, ID2, ID3)[idx_odd]
+  ) %>%
+  select(positive, negative, odd, participant_id) %>%
+  mutate(
+    positive = as.integer(positive),
+    negative = as.integer(negative),
+    odd = as.integer(odd)
+  )
+write_delim(
+  tbl_ooo_ID_save, 
+  file = "data/study1-2025-08/ooo-data-modeling.txt", 
+  col_names = FALSE
+)
 
 ## questionnaires
 cols_separate <- c("workHistory", "interests1", "interests2", "interests3", "feedback")
