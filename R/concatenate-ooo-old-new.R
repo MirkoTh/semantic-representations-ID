20384d61f6a0f24b4b7da382b8e7cb43515231d6
MirkoTh <mirkothalmann@hotmail.com>
2025-08-14 14:17:17 +0200

    save reduced data file to run testcase model

--- R/concatenate-ooo-old-new.R
+++ R/concatenate-ooo-old-new.R
@@ -1,5 +1,18 @@
+rm(list = ls())
+
 library(tidyverse)
 
+rebase_subject_ids <- function(my_tbl) {
+  my_tbl$X4 <- factor(
+    my_tbl$X4, 
+    labels = seq(0, (length(unique(my_tbl$X4)) - 1))
+  )
+  my_tbl$X4 <- as.numeric(my_tbl$X4) - 1
+  return(my_tbl)
+}
+
+
+# Load Data ---------------------------------------------------------------
 
 tbl_train_things <- read_delim("data/train_90_ID_item.txt", col_names = FALSE)
 tbl_eval_things <- read_delim("data/test_10_ID_item.txt", col_names = FALSE)
@@ -20,15 +33,13 @@ max_id_things_train <- max(tbl_train_things$X4)
 max_id_things_eval <- max(tbl_eval_things$X4)
 assertthat::are_equal(max_id_things_train,  max_id_things_eval)
 
+
+# Merge and Save Full Data Set --------------------------------------------
+
 tbl_ooo_study1$X4 <- tbl_ooo_study1$X4 + max_id_things_train
-pids_study1 <- tibble(participant_pid = unique(tbl_ooo_study1$X4))
-
-# save the updated participant ids to easily extract data after model fitting
-write_csv(pids_study1, "data/study1-2025-08/new-participant-ids-in-joint-modeling.csv")
-
-
 # full data set
 tbl_full <- rbind(tbl_train_things, tbl_eval_things, tbl_ooo_study1)
+
 
 write_delim(
   tbl_full, 
@@ -36,13 +47,48 @@ write_delim(
   col_names = FALSE
 )
 
-# small data set for testing
+pids_study1 <- tibble(
+  # ids used in modeling
+  participant_id_model = unique(tbl_ooo_study1$X4),
+  # ids only used for new study participants
+  participant_id_new = 1:length(unique(participant_id_model))) 
 
+# save the updated participant ids to easily extract data after model fitting
+write_csv(pids_study1, "data/study1-2025-08/new-participant-ids-in-joint-modeling.csv")
+
+
+
+# Save Data Frame for Testcase --------------------------------------------
+
+
+# small data set for testing
+# make sure, new ids are in testcase set (for dev purposes)
+n_samples_old <- 10
 set.seed(10)
-tbl_testcase <- tbl_full[tbl_full$X4 %in% sample(unique(tbl_full$X4), 10), ]
+tbl_testcase <- tbl_full[tbl_full$X4 %in% sample(max_id_things_train, n_samples_old), ]
+tbl_testcase <- rbind(tbl_testcase, tbl_ooo_study1)
+
+
+tbl_testcase <- rebase_subject_ids(tbl_testcase)
 
 write_delim(
   tbl_testcase, 
   file = "data/study1-2025-08/ooo_data_modeling_old_and_new_testcase.txt", 
   col_names = FALSE
-) 
+)
+
+
+# only ids from new data set
+unique_test_pids <- unique(tbl_testcase$X4)
+pids_study1_testcase <- tibble(
+  # ids used in modeling
+  participant_id_model = unique_test_pids[which(unique_test_pids >= n_samples_old)],
+  # ids only used for new study participants
+  participant_id_new = 1:length(unique(participant_id_model))
+)
+
+# save the updated participant ids to easily extract data after model fitting
+write_csv(
+  pids_study1_testcase, 
+  "data/study1-2025-08/new-participant-ids-in-joint-modeling-testcase.csv"
+)
